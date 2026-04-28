8266769d9661f0d354dc0b0f2d1879e21fd0179f
Lucas <lucas.branchu@live.fr>
2026-04-28 11:01:06 +0200

    Ajout des boutons/menus en haut

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,67 @@
 import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {Menu,ArrowDownToLine,Plus, 
+  Trash2, 
+  FileUp, 
+  FileDown} from "lucide-react"
+
 
 function App() {
   return (
     <div class="grid grid-cols-4 gap-4 auto-rows-min p-4">
 
-  <div class="col-span-1">
+  <div class="col-span-1 flex justify-start">
+    <DropdownMenu>
+      <DropdownMenuTrigger render={<Button/>}>
+        <Menu/>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuGroup>
+          <DropdownMenuLabel>Feature model</DropdownMenuLabel>
+          <DropdownMenuItem>
+            <Plus className="mr-2 h-4 w-4" />
+            <span>Nouveau</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Trash2 className="mr-2 h-4 w-4"/>
+            <span>Supprimer</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <FileUp className="mr-2 h-4 w-4" />
+            <span>Importer</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <FileDown className="mr-2 h-4 w-4" />
+            <span>Exporter</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  </div>
+
+  <div class="col-span-1 flex justify-end">
     <Button>
-      Bouton 1
+      Création
     </Button>
   </div>
 
-  <div class="col-span-1">
-    <button class="float-right bg-blue-500 text-white px-4 py-2 rounded">
-      Bouton 2
-    </button>
+  <div class="col-span-1 flex justify-start">
+    <Button>
+      Configuration
+    </Button>
   </div>
 
-  <div class="col-span-1">
-    <button class="float-left bg-blue-500 text-white px-4 py-2 rounded">
-      Bouton 3
-    </button>
-  </div>
-
-  <div class="col-span-1">
-    <button class="float-right bg-blue-500 text-white px-4 py-2 rounded">
-      Bouton 4
-    </button>
+  <div class="col-span-1 flex justify-end">
+    <Button>
+      <ArrowDownToLine />
+    </Button>
   </div>
 
   <div class="col-span-1">
