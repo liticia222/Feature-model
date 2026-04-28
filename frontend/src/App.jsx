914b20c7b8e4ceb01fd9c4993e7ad15ffbed9882
LauraChd <laura.chaduc@etud.univ-angers.fr>
2026-04-28 11:19:41 +0200

    Draggable ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,10 +7,12 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import {Menu,ArrowDownToLine,Plus, 
-  Trash2, 
-  FileUp, 
-  FileDown} from "lucide-react"
+import {
+  Menu, ArrowDownToLine, Plus,
+  Trash2,
+  FileUp,
+  FileDown
+} from "lucide-react"
 import {
   ResizableHandle,
   ResizablePanel,
@@ -18,15 +20,16 @@ import {
 } from "@/components/ui/resizable"
 import { useDraggable } from "@dnd-kit/react";
 
-function Draggable(){
+function Draggable({id, label}) {
   const { ref } = useDraggable({
-    id: "draggable",
+    id
   });
 
   return (
-    <Button ref={ref}>
-      Draggable
-    </Button>
+    <div ref={ref} className="mb-4 mr-4 p-4 bg-gray-200 rounded">
+      <p>{label ?? "Draggable"}</p>
+    </div>
+
   );
 }
 
@@ -34,80 +37,62 @@ function App() {
   return (
     <div className="grid grid-cols-4 gap-4 auto-rows-min p-4">
 
-  <div class="col-span-1 flex justify-start">
-    <DropdownMenu>
-      <DropdownMenuTrigger render={<Button/>}>
-        <Menu/>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuGroup>
-          <DropdownMenuLabel>Feature model</DropdownMenuLabel>
-          <DropdownMenuItem>
-            <Plus className="mr-2 h-4 w-4" />
-            <span>Nouveau</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Trash2 className="mr-2 h-4 w-4"/>
-            <span>Supprimer</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <FileUp className="mr-2 h-4 w-4" />
-            <span>Importer</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <FileDown className="mr-2 h-4 w-4" />
-            <span>Exporter</span>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </div>
+      <div className="col-span-1 flex justify-start">
+        <DropdownMenu>
+          <DropdownMenuTrigger render={<Button />}>
+            <Menu />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuGroup>
+              <DropdownMenuLabel>Feature model</DropdownMenuLabel>
+              <DropdownMenuItem>
+                <Plus className="mr-2 h-4 w-4" />
+                <span>Nouveau</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Trash2 className="mr-2 h-4 w-4" />
+                <span>Supprimer</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <FileUp className="mr-2 h-4 w-4" />
+                <span>Importer</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <FileDown className="mr-2 h-4 w-4" />
+                <span>Exporter</span>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
 
-  <div class="col-span-1 flex justify-end">
-    <Button>
-      Création
-    </Button>
-  </div>
-
-  <div class="col-span-1 flex justify-start">
-    <Button>
-      Configuration
-    </Button>
-  </div>
-
-  <div class="col-span-1 flex justify-end">
-    <Button>
-      <ArrowDownToLine />
-    </Button>
-  </div>
-      <div class="col-span-1">
+      <div class="col-span-1 flex justify-end">
         <Button>
-          Bouton 1
+          Création
         </Button>
       </div>
 
-      <div className="col-span-1">
-        <button className="float-right bg-blue-500 text-white px-4 py-2 rounded">
-          Bouton 2
-        </button>
+      <div class="col-span-1 flex justify-start">
+        <Button>
+          Configuration
+        </Button>
       </div>
 
-      <div className="col-span-1">
-        <button className="float-left bg-blue-500 text-white px-4 py-2 rounded">
-          Bouton 3
-        </button>
-      </div>
-
-      <div className="col-span-1">
-        <button className="float-right bg-blue-500 text-white px-4 py-2 rounded">
-          Bouton 4
-        </button>
+      <div className="col-span-1 flex justify-end">
+        <Button>
+          <ArrowDownToLine />
+        </Button>
       </div>
 
       <ResizablePanelGroup direction="horizontal" className="col-span-4 row-span-2">
         <ResizablePanel defaultSize={25}>
-            <Draggable />
-
+          <div className="grid grid-row-5 gap-4 p-4">
+            <Draggable id="draggable1" label="Draggable 1"/>
+            <Draggable id="draggable2" label="Draggable 2"/>
+            <Draggable id="draggable3" label="Draggable 3"/>
+            <Draggable id="draggable4" label="Draggable 4"/>
+            <Draggable id="draggable5" label="Draggable 5"/>
+          </div>
         </ResizablePanel>
         <ResizableHandle />
         <ResizablePanel defaultSize={75}>
