b1345460082071ba674ea66c23da6450203681d7
Lucas <lucas.branchu@live.fr>
2026-04-28 10:11:21 +0200

    ajout de shadcn-ui

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,4 @@
+import path from "path" // 1. On importe path
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
@@ -5,4 +6,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // 2. On définit l'alias
+    },
+  },
 })
